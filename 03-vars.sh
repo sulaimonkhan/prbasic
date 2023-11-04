b7290c71552aa959e81d5e52af596620c7bc9803
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-04 19:05:18 +0530

    variable

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,6 +1,6 @@
 a=10
 
-echo value of a is $a
+
 echo value of a is $a dollars
 
 
@@ -10,7 +10,7 @@ echo value of a is ${a}dollars
 
 # special variables for inputs 
 # special variables used for inputs are $0 to $n, $* , $#
-echo $0
-echo $n
-echo $*
-echo $#
+echo scrpit Name - $0
+echo First Argument - $n
+echo All Arguments- $*
+echo No of Arguments - $#
