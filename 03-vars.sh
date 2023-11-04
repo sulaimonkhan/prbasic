5e197a471d8a831d06e26fc564bff33f1fcbb7b9
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-04 18:55:24 +0530

    special vaeiabel

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,3 +6,11 @@ echo value of a is $a dollars
 
 ## Alternatively variables also can be accessed by using ${a}
 echo value of a is ${a}dollars 
+
+
+# special variables for inputs 
+# special variables used for inputs are $0 to $n, $* , $#
+echo $0
+echo $n
+echo $*
+echo $#
